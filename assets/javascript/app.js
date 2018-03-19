eb605c6cbaccb053837ddc4355f16230f6a86c8b
unknown <tberry6@gmail.com>
2018-03-19 15:17:53 -0700

    Added default answers and did some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,43 +8,52 @@ function Question(q, a, c, id) {
         var questionDisplay = $("<div class=\"question\" id=\"" + this.qid + "\">");
         questionDisplay.append("<div class=\"q-header\">" + this.question + "</div>");
         var answersDisplay = questionDisplay.append("<div id=\"" + this.qid + "-answers\">" + "</div>");
-        for(var i = 0; i < this.answers.length; i++) {
+        for (var i = 0; i < this.answers.length; i++) {
             var answer = this.answers[i];
-            answersDisplay.append("<input type=\"radio\" name = \"" + this.qid + "\" id=\"" + answer + "\"" + "value=\"" + answer + "\">");
+            var button = $("<input>");
+            button.attr("type", "radio");
+            button.attr("name", this.qid);
+            button.attr("value", answer);
+            button.attr("id", this.qid + "-" + answer);
+            if (i === 0) {
+                button.attr("checked", "");
+            }
+            answersDisplay.append(button);
             answersDisplay.append("<label for=\"" + answer + "\">" + answer + "</label>");
         }
         return questionDisplay;
     };
 }
 
-$("#questions").submit(function(event) {
+$("#questions").submit(function (event) {
     var answers = $(this).serializeArray();
+    console.log(answers);
     var numCorrect = 0;
-    for(var i = 0; i < questions.length; i++) {
-        if(questions[i].correct === answers[i].value) {
+    for (var i = 0; i < questions.length; i++) {
+        if (questions[i].correct === answers[i].value) {
             numCorrect++;
         }
     }
     $("main").html("<div id=\"result\">You got " + numCorrect + " out of " + questions.length + " questions correct!</div>");
 });
 
-var q1 = new Question("Answer is 1",["1","2","3"],"1","q1");
-var q2 = new Question("Answer is 2",["1","2","3"],"2","q2");
-var q3 = new Question("Answer is 3",["1","2","3"],"3","q3");
-var questions = [q1,q2,q3];
-for(var i = 0; i < questions.length; i++) {
+var q1 = new Question("Answer is 1", ["1", "2", "3"], "1", "q1");
+var q2 = new Question("Answer is 2", ["1", "2", "3"], "2", "q2");
+var q3 = new Question("Answer is 3", ["1", "2", "3"], "3", "q3");
+var questions = [q1, q2, q3];
+for (var i = 0; i < questions.length; i++) {
     $("#questions").append(questions[i].display());
 }
 var time = 30;
 $("#time").html(time);
 
-setInterval(function() {
-    if(time > 0) {
+setInterval(function () {
+    if (time > 0) {
         time--;
         $("#time").html(time);
     }
 }, 1000);
 
-setTimeout(function() {
+setTimeout(function () {
     $("#questions").submit();
 }, 30000);
